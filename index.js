0bb209c7809a0ba2d54030714ad9df37a8f75863
Meruem Moniker <jamesconnorhill@gmail.com>
2023-07-23 18:42:51 -0400

    f

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
         const buttons = document.querySelectorAll('button');
+        let compScore= 0;
+        let userScore= 0;
         
         buttons.forEach((button) => {
             button.addEventListener('click', function round() {
                 const playerSelection = button.value
-
-                function getComputerChoice () {
-                    const Rock = 0
-                    const Paper = 1
-                    const Scissors = 2
-                    const randNum = Math.floor(Math.random() * 3);
-                    if (randNum == Rock) {
-                        return "Rock"
-                    } else if (randNum == Paper) {
-                        return "Paper"
-                    } else if (randNum == Scissors) {
-                        return "Scissors"
-                    }
-                };
-
-                const computerSelection = getComputerChoice();
             });
         });
 
+        function getComputerChoice(){
+            const randNum = Math.floor(Math.random() * 3);
+            if (randNum == 0) {
+                return "Rock"
+            } else if (randNum == 1) {
+                return "Paper"
+            } else if (randNum == 2) {
+                return "Scissors"
+            }
+        };
+
+        const computerSelection = getComputerChoice();
+
+        function round (computerSelection, playerSelection) {
+            if (computerSelection == "Scissors" && playerSelection == "Scissors") {
+                return "It's a tie."
+            } else if (computerSelection == "Scissors" && playerSelection == "Rock") {
+                return "Rock beats scissors, you win."
+            } else if (computerSelection == "Scissors" && playerSelection == "Paper") {
+                return "Scissors beats paper, you lose."
+            } else if (computerSelection == "Rock" && playerSelection == "Rock") {
+                return "It's a tie."
+            } if (computerSelection == "Rock" && playerSelection == "Paper") {
+                return "Paper beats rock, you win"
+            } else if (computerSelection == "Rock" && playerSelection == "Scissors") {
+                return "Rock beats scissors, you lose."
+            } else if (computerSelection == "Paper" && playerSelection == "Paper") {
+                return "It's a tie."
+            } if (computerSelection == "Paper" && playerSelection == "Rock") {
+                return "Paper beats rock, you lose."
+            } if (computerSelection == "Paper" && playerSelection == "Scissors") {
+                return "Scissors beats paper, you win."
+            }
+            game()
+            
         function game () {
-            function round (computerSelection, playerSelection) {
-                if (computerSelection == "Scissors" && playerSelection == "Scissors") {
-                    return "It's a tie."
-                } else if (computerSelection == "Scissors" && playerSelection == "Rock") {
-                    return "Rock beats scissors, you win."
-                } else if (computerSelection == "Scissors" && playerSelection == "Paper") {
-                    return "Scissors beats paper, you lose."
-                } else if (computerSelection == "Rock" && playerSelection == "Rock") {
-                    return "It's a tie."
-                } if (computerSelection == "Rock" && playerSelection == "Paper") {
-                    return "Paper beats rock, you win"
-                } else if (computerSelection == "Rock" && playerSelection == "Scissors") {
-                    return "Rock beats scissors, you lose."
-                } else if (computerSelection == "Paper" && playerSelection == "Paper") {
-                    return "It's a tie."
-                } if (computerSelection == "Paper" && playerSelection == "Rock") {
-                    return "Paper beats rock, you lose."
-                } if (computerSelection == "Paper" && playerSelection == "Scissors") {
-                    return "Scissors beats paper, you win."
-                }
+        for (let i = 0; i < 1000; i++) {
+            if (userScore >= 5) {
+                return "You Win!"
+            } 
+            if (round().includes("lose")) {
+                const compScore = compScore + 1
+            } else if (round().includes("win")) {
+                const userScore = userScore++
+            };            
             }
-            for (let i = 0; i < 1000; i++) {
-                if ("lose" in round()) {
-                    const compScore = ++compScore
-                } else if ("win" in round()) {
-                    const userScore = userScore
-                };
-                if (compScore >= 5 | userScore >= 5) {
-                    return "You Win!"
-                };
-            }
-        }
+        }}
