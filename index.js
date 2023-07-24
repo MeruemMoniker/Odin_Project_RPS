b2cde4fd46bc961078dd3a18274a25074bf89c8c
Meruem Moniker <jamesconnorhill@gmail.com>
2023-07-23 22:38:24 -0400

    c

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
         const buttons = document.querySelectorAll('button');
         let compScore= 0;
         let userScore= 0;
+        const computerSelection = getComputerChoice();
+
+        function getComputerChoice(){
+            const randNum = Math.floor(Math.random() * 3);
+            if (randNum == 0) {
+                return "Rock"
+            } else if (randNum == 1) {
+                return "Paper"
+            } else if (randNum == 2) {
+                return "Scissors"
+            }
+        };
 
         function game () {
             console.log("yo")
             let arr = []
             let newarr = []
+            function round (computerSelection, playerSelection) {
+                if (computerSelection == "Scissors" && playerSelection == "Scissors") {
+                    return "It's a tie."
+                } else if (computerSelection == "Scissors" && playerSelection == "Rock") {
+                    return "Rock beats scissors, you win."
+                } else if (computerSelection == "Scissors" && playerSelection == "Paper") {
+                    return "Scissors beats paper, you lose."
+                } else if (computerSelection == "Rock" && playerSelection == "Rock") {
+                    return "It's a tie."
+                } if (computerSelection == "Rock" && playerSelection == "Paper") {
+                    return "Paper beats rock, you win"
+                } else if (computerSelection == "Rock" && playerSelection == "Scissors") {
+                    return "Rock beats scissors, you lose."
+                } else if (computerSelection == "Paper" && playerSelection == "Paper") {
+                    return "It's a tie."
+                } if (computerSelection == "Paper" && playerSelection == "Rock") {
+                    return "Paper beats rock, you lose."
+                } if (computerSelection == "Paper" && playerSelection == "Scissors") {
+                    return "Scissors beats paper, you win."
+                }
+                
+            }
             round()
         for (let i = 0; i < 50; i++) {
             if (round().includes("win")) {
@@ -33,39 +67,3 @@
                 return playerSelection
             });
         });
-
-        function getComputerChoice(){
-            const randNum = Math.floor(Math.random() * 3);
-            if (randNum == 0) {
-                return "Rock"
-            } else if (randNum == 1) {
-                return "Paper"
-            } else if (randNum == 2) {
-                return "Scissors"
-            }
-        };
-
-        const computerSelection = getComputerChoice();
-
-        function round (computerSelection, playerSelection) {
-            if (computerSelection == "Scissors" && playerSelection == "Scissors") {
-                return "It's a tie."
-            } else if (computerSelection == "Scissors" && playerSelection == "Rock") {
-                return "Rock beats scissors, you win."
-            } else if (computerSelection == "Scissors" && playerSelection == "Paper") {
-                return "Scissors beats paper, you lose."
-            } else if (computerSelection == "Rock" && playerSelection == "Rock") {
-                return "It's a tie."
-            } if (computerSelection == "Rock" && playerSelection == "Paper") {
-                return "Paper beats rock, you win"
-            } else if (computerSelection == "Rock" && playerSelection == "Scissors") {
-                return "Rock beats scissors, you lose."
-            } else if (computerSelection == "Paper" && playerSelection == "Paper") {
-                return "It's a tie."
-            } if (computerSelection == "Paper" && playerSelection == "Rock") {
-                return "Paper beats rock, you lose."
-            } if (computerSelection == "Paper" && playerSelection == "Scissors") {
-                return "Scissors beats paper, you win."
-            }
-            
-        }
