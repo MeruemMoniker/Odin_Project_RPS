d1682805a1e945665d6e6338a526676a3792e74a
Meruem Moniker <jamesconnorhill@gmail.com>
2023-08-01 21:55:44 -0400

    d

--- index.js
+++ index.js
@@ -14,13 +14,11 @@
             }
         };
 
-        
-
-        function game () {
             buttons.forEach((button) => {
-                button.addEventListener('click', function () {
+                button.addEventListener('click', () => {
                     let playerSelection = button.value;
                     console.log(playerSelection);
+                    round(computerSelection, playerSelection);
                     game();
                     return playerSelection
                 });
@@ -32,40 +30,49 @@
                 if (computerSelection == "Scissors" && playerSelection == "Scissors") {
                     return "It's a tie."
                 } else if (computerSelection == "Scissors" && playerSelection == "Rock") {
+                    userScore++
+                    roundWinner = 'player'
                     return "Rock beats scissors, you win."
                 } else if (computerSelection == "Scissors" && playerSelection == "Paper") {
+                    compScore++
+                    roundWinner = 'computer'
                     return "Scissors beats paper, you lose."
                 } else if (computerSelection == "Rock" && playerSelection == "Rock") {
                     return "It's a tie."
                 } if (computerSelection == "Rock" && playerSelection == "Paper") {
+                    userScore++
+                    roundWinner = 'player'
                     return "Paper beats rock, you win"
                 } else if (computerSelection == "Rock" && playerSelection == "Scissors") {
+                    compScore++
+                    roundWinner = 'computer'
                     return "Rock beats scissors, you lose."
                 } else if (computerSelection == "Paper" && playerSelection == "Paper") {
                     return "It's a tie."
                 } if (computerSelection == "Paper" && playerSelection == "Rock") {
+                    compScore++
+                    roundWinner = 'computer'
                     return "Paper beats rock, you lose."
                 } if (computerSelection == "Paper" && playerSelection == "Scissors") {
+                    userScore++
+                    roundWinner = 'player'
                     return "Scissors beats paper, you win."
                 }
-                
+                updateScore(roundWinner)
             }
-            let scoop = round(computerSelection, playerSelection)
-            console.log(scoop)
-        for (let i = 0; i < 50; i++) {
-            if (scoop.includes("win")) {
-                console.log("You win!")
-                arr.push(i)
-                if (arr.length >=5) {
-                return "WINNER!!!"}
-            } else if (scoop.includes("lose")) {
-                console.log("You lose!")
-                newarr.push(i)
-                if (newarr.length >=5) {
-                return "LOSER!!!"}
-            } else {
-                console.log("It's a tie!")
-                i = i - 1
+            
+
+            function updateScore(roundWinner) {
+                if (roundWinner == player) {
+                    if (userScore === 5) {
+                        return "YOU WIN!"
+                    } else if (userScore < 5) {
+                        return "You win this round, keep going!"
+                    }
+                } else {
+                    if (compScore === 5) {
+                        return "you lose..."
+                    } else if (compScore < 5) {
+                        return "You lost this round, try again!"
+                }}
             }
-            }
-        }
